4b2c68c241918a1c91d12d12e36c464523172b38
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-05-08 00:20:41 +0900

    pに統一, 乱数生成部分を修正

--- tasks/random.rb
+++ tasks/random.rb
@@ -2,15 +2,16 @@
 
 def random_group
   shuffled_members = %w[A B C D E F].shuffle
-  if rand(1..10).even?
+  rng = (1..2).to_a
+  if rng.sample.even?
     # 偶数の場合、3と3に分ける
     shuffled_members.each_slice(3) { |group| p group.sort }
   else
     # 奇数の場合、2と4に分ける、後ろから2つ除外
     group_one = shuffled_members.pop(2)
-    group_two = shuffled_members.sort.to_s
-    puts group_one.sort.to_s
-    puts group_two
+    group_two = shuffled_members.sort
+    p group_one.sort
+    p group_two
   end
 end
 
