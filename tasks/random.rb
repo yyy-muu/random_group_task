1b129c28a87738a79ed51a67cbfafcc6f75c285a
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-05-08 00:44:37 +0900

    if文の条件を変更

--- tasks/random.rb
+++ tasks/random.rb
@@ -3,11 +3,11 @@
 def random_group
   shuffled_members = %w[A B C D E F].shuffle
   rng = (1..2).to_a
-  if rng.sample.even?
-    # 偶数の場合、3と3に分ける
+  if rng.sample == 1
+    # 1が出た場合、3と3に分ける
     shuffled_members.each_slice(3) { |group| p group.sort }
   else
-    # 奇数の場合、2と4に分ける、後ろから2つ除外
+    # 2が出た場合、2と4に分ける (後ろから2つの要素を除外)
     group_one = shuffled_members.pop(2)
     group_two = shuffled_members.sort
     p group_one.sort
