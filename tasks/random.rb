00b39bec9195d42fa1dbc03b558f2b5079176d79
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-05-08 15:26:32 +0900

    条件分岐の方法を変更

--- tasks/random.rb
+++ tasks/random.rb
@@ -2,12 +2,11 @@
 
 def random_group
   shuffled_members = %w[A B C D E F].shuffle
-  rng = (1..2).to_a
-  if rng.sample == 1
-    # 1が出た場合、3と3に分ける
+  if [true, false].sample
+    # trueの場合、3と3に分ける
     shuffled_members.each_slice(3) { |group| p group.sort }
   else
-    # 2が出た場合、2と4に分ける (後ろから2つの要素を除外)
+    # falseの場合、2と4に分ける (後ろから2つの要素を除外)
     group_one = shuffled_members.pop(2)
     group_two = shuffled_members.sort
     p group_one.sort
